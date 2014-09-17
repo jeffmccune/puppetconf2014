b265e8d8b40764e5c5bac5f8df71d93882896136
Jeff McCune <jeff@puppetlabs.com>
2014-09-17 14:31:02 -0700

    Update packages to unclude ruby-devel
    Without this patch the puppet spec job doesn't work because gem native
    extensions cannot be compiled without the ruby header files in place on
    the system.  This patch addresses the problem by installing the ruby
    header files located in the ruby-devel package.

--- puppet/modules/site/manifests/packages.pp
+++ puppet/modules/site/manifests/packages.pp
@@ -3,6 +3,6 @@
 # This class manages packages on all managed nodes.
 #
 class site::packages {
-  $packages = [ 'git' ]
+  $packages = [ 'git', 'ruby', 'ruby-devel', 'rubygem-bundler', 'make', 'gcc', ]
   package { $packages: ensure => latest }
 }

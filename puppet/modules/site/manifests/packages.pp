95abd88fcb449635f7fe6fa872de380abad1d424
Jeff McCune <jeff@puppetlabs.com>
2014-09-17 14:32:52 -0700

    Add sqlite-devel package for puppet gem dependencies

--- puppet/modules/site/manifests/packages.pp
+++ puppet/modules/site/manifests/packages.pp
@@ -3,6 +3,7 @@
 # This class manages packages on all managed nodes.
 #
 class site::packages {
-  $packages = [ 'git', 'ruby', 'ruby-devel', 'rubygem-bundler', 'make', 'gcc', ]
+  $packages = [ 'git', 'ruby', 'ruby-devel', 'rubygem-bundler', 'make', 'gcc',
+                'sqlite', 'sqlite-devel' ]
   package { $packages: ensure => latest }
 }

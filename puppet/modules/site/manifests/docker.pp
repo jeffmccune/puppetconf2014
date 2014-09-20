066f98e85e1b5b3bd6c8eedef99887f00e2b6e29
Jeff McCune <jeff@puppetlabs.com>
2014-09-20 11:31:53 -0700

    Update to Docker version 1.2.0, build fa7b24f

--- puppet/modules/site/manifests/docker.pp
+++ puppet/modules/site/manifests/docker.pp
@@ -3,12 +3,22 @@
 # This class manages a minimal CentOS 7 docker host.
 #
 class site::docker {
+  $module = 'site'
+
   Package { ensure => present }
 
   package { docker: }
   ~> service { docker:
     ensure => running,
     enable => true,
+  }
+
+  file { '/usr/bin/docker':
+    ensure => file,
+    source => "puppet:///modules/${module}/docker/bin/docker",
+    owner  => 0,
+    group  => 0,
+    mode   => 755,
   }
 
   user { vagrant:
